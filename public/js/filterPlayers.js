bb445fe4311ea73ab8918e8148cd95231993915f
it-is-zane <=>
2023-06-12 22:37:59 -0700

    filter should now work for players

--- public/js/filterPlayers.js
+++ public/js/filterPlayers.js
@@ -2,7 +2,28 @@ filter = document.getElementsByClassName("filter_button")[0];
 row_items = document.getElementsByClassName("item");
 
 filter.addEventListener("click", () => {
+  first_name = document.getElementById("input-fname");
+  last_name = document.getElementById("input-lname");
+  nation = document.getElementById("input-nation");
+
   for (row in row_items) {
-    row_items.style.display = "";
+    row.style.display = "none";
+
+    // first name
+    if (first_name == "" || row.children[0].innerHTML == first_name) {
+      continue;
+    }
+
+    // last name
+    if (last_name == "" || row.children[1].innerHTML == last_name) {
+      continue;
+    }
+
+    //nation
+    if (nation == "" || row.children[2].innerHTML == nation) {
+      continue;
+    }
+
+    row.style.display = "";
   }
 });
