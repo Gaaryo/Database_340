c83075a4da215838e8ad31e0abbf762abb414045
it-is-zane <=>
2023-06-12 22:52:58 -0700

    changed the filter to hopfully work

--- public/js/filterPlayers.js
+++ public/js/filterPlayers.js
@@ -2,34 +2,33 @@ filter = document.getElementsByClassName("filter_button")[0];
 row_items = document.getElementsByClassName("item");
 
 filter.addEventListener("click", () => {
-  first_name = document.getElementById("input-fname");
-  last_name = document.getElementById("input-lname");
-  nation = document.getElementById("input-nation");
+  first_name = document.getElementById("input-fname").innerHTML;
+  last_name = document.getElementById("input-lname").innerHTML;
+  nation = document.getElementById("input-nation").innerHTML;
 
   for (let i = 0; i < row_items.length; i++) {
     console.log("a");
-    row_items[i].style.display = "none";
+    row_items[i].style.display = "";
 
     // first name
-    if (first_name == "" || row_items[i].children[0].innerHTML == first_name) {
+    if (row_items[i].children[0].innerHTML == first_name) {
       console.log("b");
       continue;
     }
 
     // last name
-    if (last_name == "" || row_items[i].children[1].innerHTML == last_name) {
+    if (row_items[i].children[1].innerHTML == last_name) {
       console.log("c");
       continue;
     }
 
     //nation
-    if (nation == "" || row_items[i].children[2].innerHTML == nation) {
+    if (row_items[i].children[2].innerHTML == nation) {
       console.log("d");
       continue;
     }
 
     console.log("e");
-
-    row_items[i].style.display = "";
+    row_items[i].style.display = "none";
   }
 });
